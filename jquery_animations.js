a623b67939582dfb1a83df022a4d65940cc1ee78
Akshaj05 <122291038+Akshaj05@users.noreply.github.com>
2023-10-29 11:53:39 +0530

    First version of the website

---
+++ jquery_animations.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+    $("#about_btn").click(function()
+    {
+        $("#about_sec").slideDown(2000);
+    });
+
+    $("#menu-icon").click(function()
+    {
+        $("#menu-list").toggle();
+    });
+});
+
+
